450557490292b2c933d0e4a203acc39a37c37982
SurveshBajaj <Sarveshbajaj9@gmail.com>
2025-01-07 00:25:47 +0530

    addition feature added

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class addition {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int res = add(5,6);
+        System.out.println(res);
+    }
+    
+}
